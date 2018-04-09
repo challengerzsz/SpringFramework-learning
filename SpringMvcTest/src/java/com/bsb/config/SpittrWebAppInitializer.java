ad2b89aee1baef5f9b4fd2f6a3dc22ecb8c0c30b
challengerZeng <664903471@qq.com>
2018-04-09 23:32:25 +0800

    Spring MVC复习

---
+++ SpringMvcTest/src/java/com/bsb/config/SpittrWebAppInitializer.java
@@ -0,0 +1,22 @@
+package com.bsb.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpittrWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        //将此Servlet映射到"/"
+        return new String[] {"/"};
+    }
+}
+
